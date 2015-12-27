cb6ab9061962ad9db317910ab62f53c37a9a7be1
dave <dave_gitlab@davepedu.com>
2015-12-26 21:41:51 -0800

    cleanup

--- common/cgi.py
+++ common/cgi.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import os
-from urllib.parse import parse_qs as _parse_qs
-from base64 import b64decode
 import sys
 import traceback
+from urllib.parse import parse_qs as _parse_qs
+from base64 import b64decode
+
 
 def start_response(content_type="text/html", status_code=("200", "OK",), extra_headers=[]):
     print('Status: %s %s' % (status_code))
@@ -23,11 +24,13 @@ def parse_qs():
         GET = {k:v[0] for k,v in GET.items()}
     return GET
 
+
 class HTTPBasicAuth:
     username = None
     password = None
     def __str__(self):
         return "<HTTPBasicAuth object username='%s' password='%s'>" % (self.username, self.password)
+
 
 def parse_auth():
     if "HTTP_AUTHORIZATION" in os.environ:
@@ -36,21 +39,5 @@ def parse_auth():
             auth = HTTPBasicAuth()
             auth.username, auth.password = b64decode(value).decode().split(":")
             return auth
-
-def fulltrace():
-    """Return the stack trace of the bot as a string"""
-    result = ""
-    result += "\n*** STACKTRACE - START ***\n"
-    code = []
-    for threadId, stack in sys._current_frames().items():
-        code.append("\n# ThreadID: %s" % threadId)
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
-            if line:
-                code.append("  %s" % (line.strip()))
-    for line in code:
-        result += line + "\n"
-    result += "\n*** STACKTRACE - END ***\n"
-    return result
 
 # cgi.print_environ()
